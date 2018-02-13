b79915f0a06a508e3217f11bbd2ab5c0d236d95c
jazzp561 <jazz.pethani@gmail.com>
2018-02-13 23:29:27 +0530

    Initial Commit

---
+++ WeatherMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherMacOS
+//
+//  Created by jasmin on 13/02/18.
+//  Copyright © 2018 jasmin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+     func applicationDidFinishLaunching(_ aNotification: Notification) {
+          // Insert code here to initialize your application
+     }
+
+     func applicationWillTerminate(_ aNotification: Notification) {
+          // Insert code here to tear down your application
+     }
+
+
+}
+
